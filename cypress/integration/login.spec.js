be14bf015ff88077bece6ec90fcc00991bf51bc3
BenHurJF <beiujeffer@hotmail.com>
2023-04-28 19:58:00 -0300

    Adicionado teste de LOGOUT com sucesso

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -12,10 +12,19 @@ describe('Cenário de Login e Logout', () => {
         cy.url().should('contain', 'inventory.html')
     });
 
-    it('Login sem sucesso', () => {
+    it('Login sem sucesso com user blocked', () => {
         cy.get('#user-name').type(Cypress.env('users')[1])
         cy.get('#password').type(Cypress.env('password'))
         cy.get('#login-button').click()
         cy.contains('h3', 'Epic sadface: Sorry, this user has been locked out.').should('be.visible')
+    });
+
+    it('Logout com sucesso', () => {
+        cy.login()
+
+        cy.get('#react-burger-menu-btn').click()
+        cy.get('#logout_sidebar_link').should('contain.text', 'Logout')
+        .click()
+        cy.url().should('eq', 'https://www.saucedemo.com/')
     });
 });
