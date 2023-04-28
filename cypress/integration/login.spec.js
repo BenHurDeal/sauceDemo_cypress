b13f6633877ab5dad7f69251e3cc1f01720307a6
BenHurJF <beiujeffer@hotmail.com>
2023-04-28 19:51:06 -0300

    Adicionado testes na spec login

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+describe('Cenário de Login e Logout', () => {
+    beforeEach(() => {
+        cy.visit('')
+    })
+
+    it('Login com sucesso', () => {
+        cy.get('#user-name').type(Cypress.env('users')[0])
+        cy.get('#password').type(Cypress.env('password'))
+        cy.get('#login-button').click()
+        cy.url().should('contain', 'inventory.html')
+    });
+
+    it('Login sem sucesso', () => {
+        cy.get('#user-name').type(Cypress.env('users')[1])
+        cy.get('#password').type(Cypress.env('password'))
+        cy.get('#login-button').click()
+        cy.contains('h3', 'Epic sadface: Sorry, this user has been locked out.').should('be.visible')
+    });
+});
